f550c05e142e2c00de23156c0ee2cc507b9a32cc
LUANA SILVA AZEVEDO <luana.azevedo@univille.br>
2025-03-17 21:47:32 -0300

    feat: revisao

--- revisao/src/main/java/br/univille/Cliente.java
+++ revisao/src/main/java/br/univille/Cliente.java
@@ -1,9 +1,21 @@
 package br.univille;
 
+
 public class Cliente {
+    public Cliente(String nome) {
+
+        this.nome = nome;
+    }
     //variável - atributo
     private String nome;
+    //polimorfismo - várias formas de existir a mesma coisa
+    public Cliente() {
 
+    }
+
+    public String toString(){
+        return getNome();
+    }
     //método
     public String getNome() {
         return nome;
@@ -12,5 +24,8 @@ public class Cliente {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    //encapsulamento = esconder a 
+    //implementação dentro do objeto
 
 }
