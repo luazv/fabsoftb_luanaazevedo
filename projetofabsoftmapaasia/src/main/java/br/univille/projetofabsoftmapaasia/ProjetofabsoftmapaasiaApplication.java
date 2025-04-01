667b8dbd47321bf8be17f51a176c37c768535677
LUANA SILVA AZEVEDO <luana.azevedo@univille.br>
2025-04-01 20:18:57 -0300

    feat:revisao

---
+++ projetofabsoftmapaasia/src/main/java/br/univille/projetofabsoftmapaasia/ProjetofabsoftmapaasiaApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabsoftmapaasia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofabsoftmapaasiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofabsoftmapaasiaApplication.class, args);
+	}
+
+}
---
+++ projetofabsoftmapaasia/src/test/java/br/univille/projetofabsoftmapaasia/ProjetofabsoftmapaasiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabsoftmapaasia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofabsoftmapaasiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
