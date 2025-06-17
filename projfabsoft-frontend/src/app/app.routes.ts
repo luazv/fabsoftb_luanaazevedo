1d27b6940d7d003274906af08f5a86c2b106dfc0
luazv <softwarebylua@gmail.com>
2025-06-17 00:52:19 +0000

    feat:usuario.component.html

--- projfabsoft-frontend/src/app/app.routes.ts
+++ projfabsoft-frontend/src/app/app.routes.ts
@@ -5,4 +5,5 @@ import { FormUsuarioComponent } from './form-usuario/form-usuario.component';
 export const routes: Routes = [
     { path: 'usuarios', component: UsuarioComponent},
     { path: 'usuarios/novo', component: FormUsuarioComponent},
+    { path: 'usuarios/alterar/:id', component: FormUsuarioComponent}
 ];
